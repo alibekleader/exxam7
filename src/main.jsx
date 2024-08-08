5b3e10e2bfe7bd595386ae6fc74a4c17cfd52ed4
alibekleader <149788620+alibekleader@users.noreply.github.com>
2024-08-08 21:08:25 +0500

    commit izohi

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-<link
-  href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-  rel="stylesheet"
-/>;
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AudioProps from "./context/index.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AudioProps>
+      <App />
+    </AudioProps>
+  </BrowserRouter>
+);
